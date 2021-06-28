1520a24dc05bb4d7d20805167e38ea65fe7db8e0
hcdl0030 <yang.li@zillnk.com>
2021-06-28 17:22:42 +0800

    update

---
+++ leon_test_package/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Leon at 6/28/2021
+
+"""
+__author__ = "Leon"
+__version__ = "0.0.1"
+__email__ = "yang.li@zillnk.com"
+
+def joke():
+    return (u'How do you tell HTML from HTML5?'
+            u'Try it out in Internet Explorer.'
+            u'Does it work?'
+            u'No?'
+            u'It\'s HTML5.')
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Leon at 6/28/2021
+
+"""
+__author__ = "Leon"
+__version__ = "0.0.1"
+__email__ = "yang.li@zillnk.com"
+from setuptools import setup
+
+setup(name='leon_test_package',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/yangli0534/leon_test_package',
+      author='yangli0534',
+      author_email='yangli0534@yahoo.com',
+      license='MIT',
+      packages=['leon_test_package'],
+      zip_safe=False)
